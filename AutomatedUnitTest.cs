3ee401778b65b5022242a03153c8cadc385a435d
jcncs <jcncs1@hotmail.com>
2021-11-14 16:43:12 +0800

    Added new tests

--- AutomatedUnitTest.cs
+++ AutomatedUnitTest.cs
@@ -1,9 +1,11 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using WebDriverManager;
 using System;
 using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Interactions;
 
 public class AutomatedUnitTest : IDisposable
 {
@@ -15,6 +17,8 @@ public class AutomatedUnitTest : IDisposable
         new DriverManager().SetUpDriver(new ChromeConfig());
         Console.WriteLine("Setup");
         _driver = new ChromeDriver(opt);
+
+        var a = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd HH:mm:ss");
     }
 
     [Test]
@@ -26,7 +30,7 @@ public class AutomatedUnitTest : IDisposable
         _driver.FindElement(By.Id("submitBtn")).Click();
         Assert.Pass();
     }
-    
+
     [Test]
     public void Access_View_Donations()
     {
@@ -37,7 +41,56 @@ public class AutomatedUnitTest : IDisposable
         _driver.Navigate().GoToUrl("https://fwms-heroku.herokuapp.com/Donations");
         Assert.Pass();
     }
-    
+
+    [Test]
+    public void Create_New_Donation()
+    {
+        _driver.Navigate().GoToUrl("https://fwms-stg.herokuapp.com/Admin/Login");
+        _driver.FindElement(By.Id("userName")).SendKeys("Admin");
+        _driver.FindElement(By.Id("pwdHash")).SendKeys("Password12345");
+        _driver.FindElement(By.Id("submitBtn")).Click();
+        _driver.Navigate().GoToUrl("https://fwms-stg.herokuapp.com/Donations/CreateDonation");
+        _driver.Manage().Window.Size = new System.Drawing.Size(1093, 694);
+        _driver.FindElement(By.Id("donationName")).Click();
+        _driver.FindElement(By.Id("donationName")).SendKeys("Test Buns");
+        _driver.FindElement(By.Id("quantity")).SendKeys("5");
+        _driver.FindElement(By.Id("ExpiryDate")).Click();
+        _driver.FindElement(By.Id("ExpiryDate")).SendKeys("2021-12-31 00:00:00");
+        IWebElement LocationSelect = _driver.FindElement(By.Name("LocationId"));
+        SelectElement selectItem = new SelectElement(LocationSelect);
+        selectItem.SelectByText("Four Seasons");
+
+        IWebElement FoodSelect = _driver.FindElement(By.Name("FoodId"));
+        SelectElement selectItem1 = new SelectElement(FoodSelect);
+        selectItem1.SelectByText("Buns");
+        _driver.FindElement(By.Id("submitBtn")).Click();
+    }
+
+    [Test]
+    public void CreateCollection()
+    {
+        _driver.Navigate().GoToUrl("https://fwms-stg.herokuapp.com/Admin/Login");
+        _driver.FindElement(By.Id("userName")).SendKeys("Admin");
+        _driver.FindElement(By.Id("pwdHash")).SendKeys("Password12345");
+        _driver.FindElement(By.Id("submitBtn")).Click();
+        _driver.Navigate().GoToUrl("https://fwms-stg.herokuapp.com/Collections/CreateCollection");
+        _driver.Manage().Window.Size = new System.Drawing.Size(1382, 744);
+        _driver.FindElement(By.Id("CollectionName")).Click();
+        _driver.FindElement(By.Id("CollectionName")).SendKeys("Test Collection");
+        _driver.FindElement(By.Id("CollectionDate")).Click();
+        _driver.FindElement(By.Id("CollectionDate")).Click();
+        _driver.FindElement(By.Id("CollectionDate")).SendKeys("2021-12-31T23:05");
+        _driver.FindElement(By.Id("CollectionDate")).SendKeys("2021-12-31T23:59");
+        _driver.FindElement(By.CssSelector(".container-fluid > form")).Click();
+        _driver.FindElement(By.Id("donationId")).Click();
+        {
+            var dropdown = _driver.FindElement(By.Id("donationId"));
+            dropdown.FindElement(By.XPath("//option[. = 'Test Bread']")).Click();
+        }
+        _driver.FindElement(By.CssSelector("option:nth-child(2)")).Click();
+        _driver.FindElement(By.CssSelector(".btn")).Click();
+    }
+
     public void Dispose()
     {
         _driver.Quit();
